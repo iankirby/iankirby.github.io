7871a2d9184460e8cbd70e7a439c7fcbdb8eef62
Ian Kirby <55120975+iankirby@users.noreply.github.com>
2023-02-19 14:55:59 -0500

    New version of the script

--- gitlab-resources/fixBib/fixBib.pl
+++ gitlab-resources/fixBib/fixBib.pl
@@ -2,28 +2,36 @@
 
 use strict; use warnings;
 
-my $fh=$ARGV[0];
+# ----------------------------
+# written by Ian Kirby.  send any errors to ian@ilkirby.com 
+# 2023-02-18 
+# ----------------------------
+
 
 # Read the .bib from standard input.  
+my $fh=$ARGV[0];
+open (my $x, $fh) or die("Could not open file $fh"); 
 
-open (my $x, $fh) or die("could not open file");
-
+# name for the output file
 my $out_fl="";
-
+# 
 if (scalar(@ARGV)==2){
     $out_fl=$ARGV[1];
-} else{
-    $out_fl="$fh";
-    $out_fl=~s/.bib/\_out.bib/;
+}
+else{$out_fl="$fh"; $out_fl=~s/.bib/\_out.bib/; # if no name for output provided, 
 }
 
-open (my $y,">",$out_fl) or die("could not open file $out_fl!");
+# print $out_fl;
+
+open (my $y,">",$out_fl) or die("Could not open file $out_fl");
+
 
 while (my $ln =<$x>){
-    #read through file to determine 
+    # read through file to determine 
     if ($ln=~m/title[=]\{[^\{]/){
         $ln=~s/\{/\{\{/;
-        $ln=~s/\}/\}\}/ if $ln=~m/[^\}]\},/;
+        $ln=~s/\}/\}\}/;# if $ln=~m/[^\}]\},/;
     }
     print $y $ln;
 }
+
